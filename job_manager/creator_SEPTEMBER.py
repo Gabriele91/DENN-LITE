d0b6f30949fedce460c9e2d5336030f5b90fc7bb
MircoT <m.tracolli@gmail.com>
2017-09-15 13:32:10 +0200

    Update September job

--- job_manager/creator_SEPTEMBER.py
+++ job_manager/creator_SEPTEMBER.py
@@ -3,9 +3,9 @@
 def main():
     EXECUTABLE = "DENN-float"
 
-    CMD = "{exe} -t 20000 -s {batch_step} -np {population_size} -f 0.5 -cr 0.9 -cmax {cmax} -cmin {cmin} -rmax {rmax} -rmin {rmin} -rc 10 -rd 0.001 -tp 16 -i {dataset} -o results/{outname} -co bin -m {mutation} -em {method} --compute_test_per_pass no -ro bench"
+    CMD = "{exe} -t 20000 -s {batch_step} -np {population_size} -f 0.5 -cr 0.9 -cmax {cmax} -cmin {cmin} -rmax {rmax} -rmin {rmin} -rc 10 -rd 0.001 -tp 17 -i {dataset} -o results/{outname} -co bin -m {mutation} -em {method} --compute_test_per_pass no -ro bench"
 
-    OUTNAME = "{method}_{dataset_name}_{batch_step}_{population_size}_{mutation}_hl{hl}_run{run}.json"
+    OUTNAME = "{method}_{dataset_name}_{s_factor}x{np_factor}_{batch_step}_{population_size}_{mutation}_hl{hl}_run{run}.json"
 
     DATASETS = [
         # (10, 10, "MAGIC", "../DENN-dataset-samples/JULY_MAGIC_1_1521x10_1s.gz"),
@@ -31,67 +31,39 @@ def main():
         # "degl"
     ]
     HLS = []
+    FACTOR = [1, 2, 4]
 
     out_names = []
 
-    with open("jobs/job_SEPTEMBER.csv", "a") as out_file:
+    with open("jobs/job_SEPTEMBER.csv", "w") as out_file:
         for features, batch_size, dataset_name, dataset in DATASETS:
             for method in METHODS:
                 for mutation in MUTATIONS:
-                    for run in range(5):
-                        hl = 0
-                        outname = OUTNAME.format(
-                            batch_step=batch_size,
-                            population_size=batch_size * 2,
-                            dataset_name=dataset_name,
-                            method=method,
-                            mutation=mutation.replace("/", ""),
-                            hl=hl,
-                            run=run
-                        )
-                        out_names.append(outname)
-                        max_val = 1.0
-                        min_val = -1.0
-                        if dataset_name == "MNIST":
-                            max_val = 0.5
-                            min_val = -0.5
-                        cur_cmd = CMD.format(
-                            exe=EXECUTABLE,
-                            batch_step=batch_size,
-                            population_size=batch_size * 2,
-                            dataset=dataset,
-                            outname=outname,
-                            method=method,
-                            mutation=mutation,
-                            cmax=max_val,
-                            cmin=min_val,
-                            rmax=max_val,
-                            rmin=min_val
-                        )
-                        out_file.write(cur_cmd)
-                        out_file.write("\n")
-        for features, batch_size, dataset_name, dataset in DATASETS:
-            for method in METHODS:
-                for mutation in MUTATIONS:
-                    for hl in HLS:
-                        for run in range(5):
-                            if dataset_name != "MNIST":
+                    for step_factor in FACTOR:
+                        for np_factor in FACTOR:
+                            for run in range(5):
+                                hl = 0
                                 outname = OUTNAME.format(
-                                    batch_step=batch_size,
-                                    population_size=batch_size * 2,
+                                    batch_step=batch_size * step_factor,
+                                    population_size=batch_size * np_factor,
                                     dataset_name=dataset_name,
                                     method=method,
                                     mutation=mutation.replace("/", ""),
                                     hl=hl,
+                                    s_factor=step_factor,
+                                    np_factor=np_factor,
                                     run=run
                                 )
                                 out_names.append(outname)
                                 max_val = 1.0
                                 min_val = -1.0
+                                if dataset_name == "MNIST":
+                                    max_val = 0.5
+                                    min_val = -0.5
                                 cur_cmd = CMD.format(
                                     exe=EXECUTABLE,
-                                    batch_step=batch_size,
-                                    population_size=batch_size * 2,
+                                    batch_step=batch_size * step_factor,
+                                    population_size=batch_size * np_factor,
                                     dataset=dataset,
                                     outname=outname,
                                     method=method,
@@ -101,17 +73,52 @@ def main():
                                     rmax=max_val,
                                     rmin=min_val
                                 )
-                                if hl > 0:
-                                    levels = [str(features * 2)] * hl
-                                    cur_cmd += " -hl {}".format(
-                                        " ".join(levels))
                                 out_file.write(cur_cmd)
                                 out_file.write("\n")
+        # for features, batch_size, dataset_name, dataset in DATASETS:
+        #     for method in METHODS:
+        #         for mutation in MUTATIONS:
+        #             for hl in HLS:
+        #                 for run in range(5):
+        #                     if dataset_name != "MNIST":
+        #                         outname = OUTNAME.format(
+        #                             batch_step=batch_size,
+        #                             population_size=batch_size * 2,
+        #                             dataset_name=dataset_name,
+        #                             method=method,
+        #                             mutation=mutation.replace("/", ""),
+        #                             hl=hl,
+        #                             run=run
+        #                         )
+        #                         out_names.append(outname)
+        #                         max_val = 1.0
+        #                         min_val = -1.0
+        #                         cur_cmd = CMD.format(
+        #                             exe=EXECUTABLE,
+        #                             batch_step=batch_size,
+        #                             population_size=batch_size * 2,
+        #                             dataset=dataset,
+        #                             outname=outname,
+        #                             method=method,
+        #                             mutation=mutation,
+        #                             cmax=max_val,
+        #                             cmin=min_val,
+        #                             rmax=max_val,
+        #                             rmin=min_val
+        #                         )
+        #                         if hl > 0:
+        #                             levels = [str(features * 2)] * hl
+        #                             cur_cmd += " -hl {}".format(
+        #                                 " ".join(levels))
+        #                         out_file.write(cur_cmd)
+        #                         out_file.write("\n")
 
     ##
     # Check if all out names are differents
     assert len(out_names) == len(set(out_names)), "{} != {}".format(
         len(out_names), len(set(out_names)))
+    
+    print(len(out_names))
 
 
 if __name__ == '__main__':
