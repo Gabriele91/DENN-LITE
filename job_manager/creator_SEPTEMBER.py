9453932ccdfffb71745982dbea477189b1993d2a
MircoT <m.tracolli@gmail.com>
2017-09-22 13:58:43 +0200

    Update creator

--- job_manager/creator_SEPTEMBER.py
+++ job_manager/creator_SEPTEMBER.py
@@ -8,73 +8,75 @@ def main():
     OUTNAME = "{method}_{dataset_name}_{batch_size}_{batch_step}_{population_size}_{mutation}_hl{hl}_run{run}.json"
 
     DATASETS = [
-        # (10, 10, "MAGIC"),
-        # # (10, 20, "MAGIC"),
-        # # (10, 40, "MAGIC"),
-        # (41, 10, "QSAR"),
-        ## (41, 20, "QSAR"),
-        ## (41, 40, "QSAR"),
-        # (19, 10, "BANK"),
-        ## (19, 20, "BANK"),
-        ## (19, 40, "BANK"),
-        # (128, 30, "GASS"),
-        ## (128, 60, "GASS"),
-        ## (128, 120, "GASS"),
-        (784, 50, "MNIST"),
-        ## (784, 100, "MNIST"),
-        ## (784, 200, "MNIST"),
+        (10, 2, "MAGIC"),
+        (41, 2, "QSAR"),
+        (128, 6, "GASS"),
+        # (784, 10, "MNIST")
     ]
 
     DATASETS_DICT = {
         "MAGIC": {
             10: "../DENN-dataset-samples/JULY_MAGIC_1_1521x10_1s.gz",
             20: "../DENN-dataset-samples/JULY_MAGIC_1_760x20_1s.gz",
-            40: "../DENN-dataset-samples/JULY_MAGIC_1_380x40_1s.gz"
+            40: "../DENN-dataset-samples/JULY_MAGIC_1_380x40_1s.gz",
+            60: "../DENN-dataset-samples/JULY_MAGIC_1_253x60_1s.gz",
+            100: "../DENN-dataset-samples/JULY_MAGIC_1_152x100_1s.gz"
         },
         "QSAR": {
             10: "../DENN-dataset-samples/JULY_QSAR_1_84x10_1s.gz",
             20: "../DENN-dataset-samples/JULY_QSAR_1_42x20_1s.gz",
-            40: "../DENN-dataset-samples/JULY_QSAR_1_21x40_1s.gz"
+            40: "../DENN-dataset-samples/JULY_QSAR_1_21x40_1s.gz",
+            60: "../DENN-dataset-samples/JULY_QSAR_1_14x60_1s.gz",
+            100: "../DENN-dataset-samples/JULY_QSAR_1_8x100_1s.gz"
         },
         "GASS": {
             30: "../DENN-dataset-samples/JULY_GasSensorArrayDrift_1_370x30_1s.gz",
             60: "../DENN-dataset-samples/JULY_GasSensorArrayDrift_1_185x60_1s.gz",
-            120: "../DENN-dataset-samples/JULY_GasSensorArrayDrift_1_92x120_1s.gz"
+            120: "../DENN-dataset-samples/JULY_GasSensorArrayDrift_1_92x120_1s.gz",
+            180: "../DENN-dataset-samples/JULY_GasSensorArrayDrift_1_61x180_1s.gz",
+            300: "../DENN-dataset-samples/JULY_GasSensorArrayDrift_1_37x300_1s.gz"
         },
         "MNIST": {
             50: "../DENN-dataset-samples/JULY_MNIST_1_1080x50_1s.gz",
             100: "../DENN-dataset-samples/JULY_MNIST_1_540x100_1s.gz",
-            200: "../DENN-dataset-samples/JULY_MNIST_1_270x200_1s.gz"
+            200: "../DENN-dataset-samples/JULY_MNIST_1_270x200_1s.gz",
+            300: "../DENN-dataset-samples/JULY_MNIST_1_180x300_1s.gz",
+            500: "../DENN-dataset-samples/JULY_MNIST_1_108x500_1s.gz"
         }
     }
 
     METHODS = ["JDE"]
     MUTATIONS = [
-        "rand/1",
-        # "degl"
+        # "rand/1",
+        "degl"
     ]
     HLS = []
-    FACTORS = [1, 2, 4]
+    BATCH_FACTORS = [ 
+        # 5, 10, 20
+        30, 50 
+    ]
+    STEP_FACTORS = [ 5, 10, 20 ]
+    NP_FACTORS = [ 10, 20, 40 ]
 
     out_names = []
 
     with open("jobs/job_SEPTEMBER.csv", "w") as out_file:
-        for features, batch_size, dataset_name in DATASETS:
-            for batch_factor in FACTORS:
-                for step_factor in FACTORS:
-                    for np_factor in [elm * 2 for elm in FACTORS]:
+        for n_features, n_classes, dataset_name in DATASETS:
+            for batch_factor in BATCH_FACTORS:
+                for step_factor in STEP_FACTORS:
+                    for np_factor in NP_FACTORS:
                         for method in METHODS:
                             for mutation in MUTATIONS:
                                 for run in range(5):
                                     hl = 0
                                     outname = OUTNAME.format(
-                                        batch_step=batch_size * step_factor,
-                                        population_size=batch_size * np_factor,
+                                        batch_step=n_classes * step_factor,
+                                        population_size=n_classes * np_factor,
                                         dataset_name=dataset_name,
                                         method=method,
                                         mutation=mutation.replace("/", ""),
                                         hl=hl,
-                                        batch_size=batch_size * batch_factor,
+                                        batch_size=n_classes * batch_factor,
                                         run=run
                                     )
                                     out_names.append(outname)
@@ -85,10 +87,10 @@ def main():
                                         min_val = -0.5
                                     cur_cmd = CMD.format(
                                         exe=EXECUTABLE,
-                                        batch_step=batch_size * step_factor,
-                                        population_size=batch_size * np_factor,
+                                        batch_step=n_classes * step_factor,
+                                        population_size=n_classes * np_factor,
                                         dataset=DATASETS_DICT[dataset_name][
-                                            batch_size * batch_factor],
+                                            n_classes * batch_factor],
                                         outname=outname,
                                         method=method,
                                         mutation=mutation,
