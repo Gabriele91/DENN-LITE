e4244db58dc3b76f56de8bcaa6bd662e6320142f
Gabriele91 <dbgabri@gmail.com>
2017-12-20 15:22:33 +0100

    Added distance function between individuals

--- include/DennLayer.h
+++ include/DennLayer.h
@@ -4,58 +4,73 @@
 
 namespace Denn
 {
-	class Layer : public std::enable_shared_from_this< Layer >
-	{ 
+class Layer : public std::enable_shared_from_this< Layer >
+{ 
+public:
+	//ref to Layer
+	using Scalar = Denn::Scalar;
+	using SPtr   = std::shared_ptr<Layer>;
+	//return ptr
+	SPtr get_ptr();
+	///////////////////////////////////////////////////////////////////////////
+	//EIGEN_MAKE_ALIGNED_OPERATOR_NEW	
+	///////////////////////////////////////////////////////////////////////////
+	virtual Layer::SPtr copy() const  				                                                 = 0;
+	virtual Matrix apply(const Matrix& input) const								                     = 0;
+	virtual size_t size() const											                             = 0;
+	virtual Matrix& operator[](size_t i)								                             = 0;
+	virtual const Matrix& operator[](size_t i) const						                         = 0;		
+	virtual ActivationFunction get_activation_function()											 = 0;
+	virtual void           set_activation_function(ActivationFunction active_function)				 = 0;
+	///////////////////////////////////////////////////////////////////////////
+	//Backpropagation stuff
+	virtual Matrix              feedforward(const Matrix& input, Matrix& linear_out)				  							   = 0;
+	virtual Matrix              backpropagate_delta(const Matrix& loss)     							                           = 0;		
+	virtual Matrix              backpropagate_derive(const Matrix& delta, const Matrix& linear_out)       			               = 0;
+	virtual std::vector<Matrix> backpropagate_gradient(const Matrix& delta, const Matrix& linear_input, Scalar regular=Scalar(0.0))= 0;
+	///////////////////////////////////////////////////////////////////////////
+	class Iterator 
+	{
 	public:
-		//ref to Layer
-		using SPtr = std::shared_ptr<Layer>;
-		//return ptr
-		SPtr get_ptr();
-		///////////////////////////////////////////////////////////////////////////
-		//EIGEN_MAKE_ALIGNED_OPERATOR_NEW	
-		///////////////////////////////////////////////////////////////////////////
-		virtual Layer::SPtr copy() const  				                                                 = 0;
-		virtual Matrix apply(const Matrix& input) const								                     = 0;
-		virtual size_t size() const											                             = 0;
-		virtual Matrix& operator[](size_t i)								                             = 0;
-		virtual const Matrix& operator[](size_t i) const						                         = 0;		
-		virtual ActivationFunction get_activation_function()											 = 0;
-		virtual void           set_activation_function(ActivationFunction active_function)				 = 0;
-		///////////////////////////////////////////////////////////////////////////
-		//Backpropagation stuff
-		virtual Matrix              feedforward(const Matrix& input, Matrix& linear_out)				  							   = 0;
-		virtual Matrix              backpropagate_delta(const Matrix& loss)     							                           = 0;		
-		virtual Matrix              backpropagate_derive(const Matrix& delta, const Matrix& linear_out)       			               = 0;
-		virtual std::vector<Matrix> backpropagate_gradient(const Matrix& delta, const Matrix& linear_input, Scalar regular=Scalar(0.0))= 0;
-		///////////////////////////////////////////////////////////////////////////
-		class Iterator 
-		{
-		public:
 
-			Iterator(const Iterator& it);
-			Iterator(Layer& layer, size_t index);
-			Iterator(const Layer& layer, size_t index);
-	
-			Iterator& operator++();
-			Iterator operator++(int);
-			
-			bool operator==(const Iterator& rhs) const;
-			bool operator!=(const Iterator& rhs) const;
+		Iterator(const Iterator& it);
+		Iterator(Layer& layer, size_t index);
+		Iterator(const Layer& layer, size_t index);
 
-			Matrix& operator*();
-			const Matrix& operator*() const;
+		Iterator& operator++();
+		Iterator operator++(int);
+		
+		bool operator==(const Iterator& rhs) const;
+		bool operator!=(const Iterator& rhs) const;
 
-		protected:
+		Matrix& operator*();
+		const Matrix& operator*() const;
 
-			friend class Denn::Layer;
-			Layer* m_layer;
-			size_t m_index;
-		};
+	protected:
 
-		Iterator begin();
-		Iterator end();
-		const Iterator begin() const;
-		const Iterator end()   const;
-		///////////////////////////////////////////////////////////////////////////
+		friend class Denn::Layer;
+		Layer* m_layer;
+		size_t m_index;
 	};
+
+	Iterator begin();
+	Iterator end();
+	const Iterator begin() const;
+	const Iterator end()   const;
+	///////////////////////////////////////////////////////////////////////////
+};
+
+template <>
+inline Layer::Scalar distance_pow2<Layer>(const Layer& a, const Layer& b)
+{
+	//bad case
+	if(a.size()!=b.size()) return std::numeric_limits<Scalar>::infinity();
+	//value
+	Scalar dpow2 = 0.0;
+	//sum
+	for(size_t i = 0; i!=a.size() ; ++i) dpow2 += distance_pow2(a[i],b[i]);
+	//return 
+	return dpow2;
+} 
+
 }
--- include/DennNeuralNetwork.h
+++ include/DennNeuralNetwork.h
@@ -8,6 +8,7 @@ class NeuralNetwork
 {
 public:
 	////////////////////////////////////////////////////////////////
+	using Scalar			 = Denn::Scalar;
 	using LayerList			 = std::vector < Layer::SPtr >;
 	using LayerIterator		 = typename LayerList::iterator;
 	using LayerConstIterator = typename LayerList::const_iterator;
@@ -73,6 +74,18 @@ protected:
 
 };
 
+template <>
+inline NeuralNetwork::Scalar distance_pow2<NeuralNetwork>(const NeuralNetwork& a, const NeuralNetwork& b)
+{
+	//bad case
+	if(a.size()!=b.size()) return std::numeric_limits<Scalar>::infinity();
+	//value
+	Scalar dpow2 = 0.0;
+	//sum
+	for(size_t i = 0; i!=a.size() ; ++i) dpow2 += distance_pow2(a[i],b[i]);
+	//return 
+	return dpow2;
+} 
 
 NeuralNetwork::BackpropagationDelta operator + (  const NeuralNetwork::BackpropagationDelta& left
 											    , const NeuralNetwork::BackpropagationDelta& right);
