a2f455a9a122a064c93e427f2fc6767ef5a086bd
Gabriele91 <dbgabri@gmail.com>
2018-02-05 14:54:34 +0100

    Added DennNRamEval.cpp

---
+++ source/DennNRamEval.cpp
@@ -0,0 +1,33 @@
+#include "DennNRam.h"
+
+namespace Denn
+{
+namespace NRam 
+{
+    ////////////////////////////////////////////////////////////////////////////////////////
+	//Evaluetor of nram
+	REGISTERED_EVALUATION(NRamEval, "nram")
+	//methods
+	bool NRamEval::minimize() const { return true; }
+	Scalar NRamEval::operator () (const Individual& individual, const DataSet& dataset)
+	{
+		denn_assert(m_context);
+		//network
+		auto& nn = individual.m_network;
+		//Dataset
+		auto& in_mem = dataset.features();
+		auto& out_mem = dataset.labels();
+		auto& cost_mask = dataset.mask();
+        auto& max_int = dataset.get_metadata("max_int").get<int>();
+        auto& timesteps = dataset.get_metadata("timesteps").get<int>();
+		//execute
+        return NRam::train(*m_context, nn, in_mem, out_mem, cost_mask, max_int, timesteps);
+	}
+	//set context
+	Evaluation::SPtr NRamEval::set_context(const NRamLayout& context)
+	{
+		m_context = &context;
+		return this->get_ptr();
+	}
+}
+}
