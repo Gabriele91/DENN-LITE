6498dc26e2af5e2745c676f708cf724e562d5cac
Valerio Belli <belli.valerio@gmail.com>
2018-03-18 13:31:16 +0100

    Fix

--- source/DennNRamEval.cpp
+++ source/DennNRamEval.cpp
@@ -24,30 +24,29 @@ namespace NRam
 
 		const size_t batch_size = m_context->m_batch_size;
 
-    	auto& max_int = dataset.get_metadata("max_int").get<int>();
-   	 	auto& timesteps = dataset.get_metadata("time_steps").get<int>();
-		auto& train_in_mem = Matrix::Zero(0, 0);
-		auto& test_in_mem = Matrix::Zero(0, 0);
-		auto& train_out_mem = Matrix::Zero(0, 0);
-		auto& test_out_mem = Matrix::Zero(0, 0);
+    	int& max_int = dataset.get_metadata("max_int").get<int>();
+   	 	int& timesteps = dataset.get_metadata("time_steps").get<int>();
+		Matrix train_in_mem = Matrix::Zero(0, 0);
+		Matrix test_in_mem = Matrix::Zero(0, 0);
+		Matrix train_out_mem = Matrix::Zero(0, 0);
+		Matrix test_out_mem = Matrix::Zero(0, 0);
 		if (m_context->m_activate_curriculum_learning)
 		{
-			auto& train_in_mem = in_mem.block(0, 0, batch_size - (batch_size / 10), max_int);
-			auto& test_in_mem = in_mem.block(batch_size - (batch_size / 10), 0, (batch_size / 10), max_int);
-			auto& train_out_mem = out_mem.block(0, 0, batch_size - (batch_size / 10), max_int);
-			auto& test_out_mem = out_mem.block(batch_size - (batch_size / 10), 0, (batch_size / 10), max_int);
+			train_in_mem = in_mem.block(0, 0, batch_size - (batch_size / 10), max_int);
+			test_in_mem = in_mem.block(batch_size - (batch_size / 10), 0, (batch_size / 10), max_int);
+			train_out_mem = out_mem.block(0, 0, batch_size - (batch_size / 10), max_int);
+			test_out_mem = out_mem.block(batch_size - (batch_size / 10), 0, (batch_size / 10), max_int);
 		}
 		else
 		{
-			auto& train_in_mem = in_mem;
-			auto& train_out_mem = out_mem;
+			train_in_mem = in_mem;
+			train_out_mem = out_mem;
 		}
 		auto& cost_mask = dataset.mask();
-        auto& error_m = dataset.mask_error();
+    	auto& error_m = dataset.mask_error();
 
 		// Execute
-		Scalar train_result =
-			NRam::train(*m_context, nn, train_in_mem, train_out_mem, cost_mask, max_int, timesteps);
+		Scalar train_result = NRam::train(*m_context, nn, train_in_mem, train_out_mem, cost_mask, max_int, timesteps);
 		
 		// Set error rate
 		if (m_context->m_activate_curriculum_learning)
