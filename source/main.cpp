c73ba599d27b5704358a9e98f44b889b04e65d43
Valerio Belli <belli.valerio@gmail.com>
2018-03-26 18:18:26 +0200

    Fix tasks

--- source/main.cpp
+++ source/main.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <utility>
 #include "Denn.h"
+#include "DennDump.h"
 
 int main(int argc,const char** argv)
 {
@@ -30,5 +31,22 @@ int main(int argc,const char** argv)
     if (!instance)            return 128;
     if (!instance->execute()) return 255;
     ////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // Random				  m_random_engine;
+    // Denn::NRam::Task::SPtr task = Denn::NRam::TaskFactory::create(parameters.m_task, 2, parameters.m_max_int, parameters.m_n_registers, 
+    //      parameters.m_time_steps, parameters.m_sequence_size, 0, 0, 
+    //         100, 0.05, m_random_engine
+    //     );
+    
+    // auto batch = task->create_batch(0, Scalar(0.0));
+    // auto& in_mem = std::get<0>(batch);
+    // auto& out_mem = std::get<1>(batch);
+    // auto& mask = std::get<2>(batch);
+    // auto& error_mask = std::get<5>(batch);
+    // MESSAGE(Dump::json_matrix(in_mem))
+    // MESSAGE(Dump::json_matrix(out_mem))
+    // MESSAGE(Dump::json_matrix(mask))
+    // MESSAGE(Dump::json_matrix(error_mask))
+    //lol 1 2 3 4
     return 0;
 }
